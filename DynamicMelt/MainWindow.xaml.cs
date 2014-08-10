a7c388d87b4a5c91e91dea5066fc41d2015f11f3
Dmdv <dimos-d@yandex.ru>
2014-08-11 02:33:43 +0400

    First commit

---
+++ DynamicMelt/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Navigation;
+
+namespace DynamicMelt
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : NavigationWindow
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
