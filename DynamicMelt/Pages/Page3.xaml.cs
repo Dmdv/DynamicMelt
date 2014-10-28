e1ab756785215e303c7613e36b05be4a4dd28c52
Dmdv <dimos-d@yandex.ru>
2014-10-28 18:48:35 +0300

    Added Page3 and ComponentAnountControl
    Added Page3 and ComponentAnountControl

---
+++ DynamicMelt/Pages/Page3.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DynamicMelt
+{
+	public partial class Page3
+	{
+		public Page3()
+		{
+			this.InitializeComponent();
+
+			// Insert code required on object creation below this point.
+		}
+	}
+}
